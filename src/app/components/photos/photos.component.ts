9a752a0f4752fbcda93e550b05ac9f22e247e18e
Tobias Abarbanell <tobias@abarbanell.de>
2016-10-06 20:54:00 +0200

    link to big pictures

--- src/app/components/photos/photos.component.ts
+++ src/app/components/photos/photos.component.ts
@@ -16,7 +16,7 @@ import {PhotoService} from '../../services/photos.service';
             <tr *ngFor="let photo of photos">
             <td> {{photo.name}} </td>
             <td> {{photo.createdTime}} </td>
-            <td> <img src="{{photo.thumbnailLink}}"> </td>
+            <td> <a href="{{photo.webViewLink}}"><img src="{{photo.thumbnailLink}}" ></a> </td>
             <td> {{photo.imageMediaMetadata.cameraMake}} {{photo.imageMediaMetadata.cameraModel}} </td>
             <td> {{photo.imageMediaMetadata.location.altitude}} m </td>
             </tr>
@@ -28,15 +28,18 @@ import {PhotoService} from '../../services/photos.service';
 export class PhotosComponent {
     errorMessage: string;
     photos=[];
+
     constructor(private photoService: PhotoService) {}
 
-    ngOnInit() { this.getPhotos();}
+    ngOnInit() { 
+        this.getPhotos();
+    }
 
     getPhotos() {
-            this.photoService.getPhotos().subscribe(
-                result => { this.photos = result},
-                error => { this.errorMessage = <any>error}
-            )
+        this.photoService.getPhotos().subscribe(
+            result => { this.photos = result },
+            error => { this.errorMessage = <any>error }
+        )
     }
 
 }  
